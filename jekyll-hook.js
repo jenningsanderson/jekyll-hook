a2db4478dd6b45bbaec48e1eca1bb9b8372f0777
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-23 17:08:20 -0700

    updated the config

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -29,7 +29,7 @@ app.get('/rake', function(req, res){
     console.log('Got task: '+rakeTask.toString()+' for '+site.toString()+' site');
 
     //This is where it runs the rake task
-    res.send(config.site.path);
+    res.send(config.sites[site].path);
 
     // run(site, , function(err) {
     //         if (err) {
