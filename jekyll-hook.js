5b9c3e917dbd2e43df33454993fe18a51105940f
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-23 18:44:57 -0700

    updated again

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
     var site = req.query.site;
     var task = 'cd' + config.sites[site].repo + '; rake -T';
 
-    res.send(task);
+    res.send(spawn(task));
 });
 
 //query is GET data.
