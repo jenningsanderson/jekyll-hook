2ea14cbd4f59eb98588b91fc79bc1616ecfc5912
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-24 17:44:54 -0700

    updated agin

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -24,13 +24,8 @@ app.get('/', function(req, res){
     if (params.length > 0){
         var command = run('./scripts/rake_tasks.sh', params, {stdio: 'inherit' });
         
-        var output = [];
+        var output = command.stdout;
         
-        //Get the output
-        command.stdout.on('data', function(chunk) {
-            output.push(chunk);
-        });
-
         // Done running scripts
         console.log('Successfully ran rake listing');
         res.send(output);
