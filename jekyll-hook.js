394ddc42136a54d84c54e185e118c8ef830d5e82
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-24 17:19:13 -0700

    this again

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
         }
     }
 
-    if (params.length > 0){}
+    if (params.length > 0){
         var task = 'cd ' + repo + '; rake -T';
         var shellResponse = spawn('./scripts/rake_tasks.sh', params, function(err) {
              if (err) {
