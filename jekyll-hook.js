9e83e562122fdde40508134706f1aab05de4bf7b
Jennings Anderson <jennings.anderson@colorado.edu>
2014-01-26 12:16:29 -0700

    updated this again

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -154,8 +154,8 @@ function convertToHTML(chunk, site){
     var lineArray = chunk.split('\n');
     html = '</html><body><h1>This is the edit page</h1><ul>'
     for (var i = 0; i < lineArray.length-1; i++) {
-        rakeTask = lineArray[i].substr(5,lineArray[i].indexOf('#')-5).trim();
-        html += '<li>' + lineArray[i].link('/rake?site='+site+'&task='+rakeTask.replace(/[arg1]/,'')) + '</li>';
+        rakeTask = lineArray[i].substr(5,lineArray[i].indexOf('#')-5).trim().replace("arg1",'');
+        html += '<li>' + lineArray[i].link('/rake?site='+site+'&task='+rakeTask + '</li>';
     }
     return html+'</ul>';
 }
