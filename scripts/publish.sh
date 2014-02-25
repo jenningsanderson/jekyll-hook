d744faf1bcfd91e43fcb970b45a9117b96b6dc85
Jennings Anderson <jennings.anderson@colorado.edu>
2014-02-24 22:20:53 -0700

    It's going to do a fetch and hard reset not

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,8 +4,9 @@ set -e
 #Move to the directory
 cd $1
 
-#Pull from Github
-git pull origin master
+#Pull from Github ( With Force! )
+git fetch --all
+git reset --hard origin/master
 
 #Do the fullbuild
 rake fullbuild
